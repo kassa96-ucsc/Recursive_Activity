98c6503c847d466c0703d41d63cf8985c46be59d
kassa96-ucsc <56383141+kassa96-ucsc@users.noreply.github.com>
2021-07-18 21:09:48 +0530

    Create Addition.scala

---
+++ Addition.scala
@@ -0,0 +1,16 @@
+object Addition extends App{
+
+    def total(x:Int):Int = {
+    
+        if(x==1) {
+            1
+        }
+        else{
+            x+total(x-1)
+        }
+
+    }
+
+    print(total(5))
+
+}
